0ee7d087c750ed2ffdacb0868c2b52efdf4e910b
Charles Maheu <CHMAH33@ulaval.ca>
2018-12-03 11:52:35 -0500

    fixed treeshaking

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
-import { ComponentsPlugin, DefaultSpritesPlugin, DirectivesPlugin, FiltersPlugin, FRENCH, FrenchPlugin, UtilsPlugin } from '@ulaval/modul-components';
+
 import '@ulaval/modul-components/dist/styles/main.scss';
+import '@ulaval/modul-components/dist/utils/polyfills';
 import Vue from 'vue';
 import App from './app';
+import ModulPlugin from './modul';
 
 Vue.config.productionTip = false;
 
-Vue.use(UtilsPlugin,
-    {
-        propagateVueParserErrors: true, i18PluginOptions: { curLang: FRENCH }
-    }); // propagateVueError to console and use french
-Vue.use(ComponentsPlugin, { richTextOptions: { key: 'froela_liscence_key' } }); // Use fake key to avoid error
-Vue.use(DirectivesPlugin);
-Vue.use(FiltersPlugin);
-Vue.use(FrenchPlugin);
-Vue.use(DefaultSpritesPlugin);
+Vue.use(ModulPlugin);
 
 const vue = new Vue({
     render: (h) => h(App)

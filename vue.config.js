92eccfa1bf54bedb74cc992946b1faf22653b3a6
Charles Maheu <CHMAH33@ulaval.ca>
2018-12-05 09:57:12 -0500

    added compression plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const StyleLintPlugin = require('stylelint-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
     lintOnSave: false,
@@ -14,72 +15,73 @@ module.exports = {
                 configFile: '.stylelintrc',
                 emitErrors: true
             }),
+            new CompressionPlugin()
         ],
         module: {
             rules: [{
-                    test: /\.js$/,
-                    use: ["source-map-loader"],
-                    enforce: "pre"
-                },
-                {
-                    enforce: 'pre',
-                    test: /\.scss$/,
-                    loader: "sass-loader",
-                    options: {
-                        sourceMap: true,
-                        includePaths: ["./node_modules/@ulaval/modul-components/dist/styles"]
-                    }
-                },
-                {
-                    enforce: 'post',
-                    test: /\.scss$/,
-                    use: ['style-loader',
-                        'css-loader',
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                sourceMap: true,
-                                plugins: function () {
-                                    return [
-                                        require('autoprefixer')
-                                    ];
-                                }
+                test: /\.js$/,
+                use: ["source-map-loader"],
+                enforce: "pre"
+            },
+            {
+                enforce: 'pre',
+                test: /\.scss$/,
+                loader: "sass-loader",
+                options: {
+                    sourceMap: true,
+                    includePaths: ["./node_modules/@ulaval/modul-components/dist/styles"]
+                }
+            },
+            {
+                enforce: 'post',
+                test: /\.scss$/,
+                use: ['style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')
+                                ];
                             }
                         }
-                    ]
-                },
-                {
-                    test: /.html$/,
-                    loader: "vue-template-loader",
-                    include: [
-                        /node_modules/,
-                        /src/
-                    ],
-                    options: {
-                        scoped: true
                     }
-                },
-                {
-                    test: /\.svg$/,
-                    loader: 'svg-inline-loader',
-                    options: {
-                        removeTags: true,
-                        removingTags: ['desc', 'defs', 'style'],
-                        removeSVGTagAttrs: true
-                    }
-                },
-                {
-                    test: /\.ts$/,
-                    enforce: 'pre',
-                    loader: 'tslint-loader',
-                    include: [/src/],
-                    options: {
-                        configFile: 'tslint.json',
-                        formatter: 'grouped',
-                        formattersDirectory: 'node_modules/custom-tslint-formatters/formatters',
-                        emitErrors: true
-                    }
+                ]
+            },
+            {
+                test: /.html$/,
+                loader: "vue-template-loader",
+                include: [
+                    /node_modules/,
+                    /src/
+                ],
+                options: {
+                    scoped: true
                 }
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader',
+                options: {
+                    removeTags: true,
+                    removingTags: ['desc', 'defs', 'style'],
+                    removeSVGTagAttrs: true
+                }
+            },
+            {
+                test: /\.ts$/,
+                enforce: 'pre',
+                loader: 'tslint-loader',
+                include: [/src/],
+                options: {
+                    configFile: 'tslint.json',
+                    formatter: 'grouped',
+                    formattersDirectory: 'node_modules/custom-tslint-formatters/formatters',
+                    emitErrors: true
+                }
+            }
             ]
         },
     },
